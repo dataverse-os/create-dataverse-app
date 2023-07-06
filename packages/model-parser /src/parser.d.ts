78a85779e686ac93e828cb518cc7863fe5792a9c
Canvas Lee <746591811@qq.com>
2023-07-06 18:28:42 +0800

    chore: delete compiled js in src

--- packages/model-parser /src/parser.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model, Output } from "./types";
-export declare class ModelParser {
-    private _output;
-    constructor(output: Output);
-    get output(): Output;
-    get appName(): string;
-    get appSlug(): string;
-    getModelByName(modelName: string): Model;
-}
